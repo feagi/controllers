614965c4516a62e4eb9dcee75326142bd57d0d0f
kakcalu13 <kevin.a.araujo@gmail.com>
2024-12-26 11:07:08 -0500

    updated for pip package

--- simulators/mujoco/feagi_mujoco/feagi_connector_mujoco/__main__.py
+++ simulators/mujoco/feagi_mujoco/feagi_connector_mujoco/__main__.py
@@ -3,20 +3,18 @@
 
 import traceback
 from time import sleep
-import feagi_connector_mujoco import controller as feagi_controller_mujoco
+import feagi_connector_mujoco
 from feagi_connector import feagi_interface as feagi
+from feagi_connector_mujoco import controller as feagi_controller_mujoco
 
 
 if __name__ == '__main__':
     current_path = feagi_connector_mujoco.__path__
-    feagi.validate_requirements(str(current_path[0]) + '/requirements.txt')  # you should get it from the boilerplate generator
-
-    while True:
-        try:
-            feagi_controller_mujoco.start()
-            sleep(5)
-        except Exception as e:
-            print(f"Controller run failed", e)
-            traceback.print_exc()
-            sleep(2)
+    feagi.validate_requirements(str(current_path[0]) + '/requirements.txt')
+    try:
+        feagi_controller_mujoco.start(current_path[0] + '/')
+        sleep(5)
+    except Exception as e:
+        print(f"Controller run failed", e)
+        traceback.print_exc()
 
