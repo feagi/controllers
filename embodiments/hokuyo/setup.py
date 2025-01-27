d906e5713768203354269f5b71c337dbd720b91c
kakcalu13 <kevin.a.araujo@gmail.com>
2025-01-27 17:27:36 -0500

    clean up

--- embodiments/hokuyo/setup.py
+++ embodiments/hokuyo/setup.py
@@ -1,40 +1,33 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 from glob import glob
-import subprocess, os, platform
-
+import os
 
 package_name = 'hokuyo'
 
 setup(
     name=package_name,
-    version='2.3.2',
-    packages=[package_name],
+    version='0.0.0',
+    packages=find_packages(),
     data_files=[
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
         (os.path.join('share', package_name, 'launch'), glob('launch/*.py')),
+        (os.path.join('share', package_name, 'config'), [
+            'src/networking.json',
+            'src/capabilities.json',
+        ]),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
-    maintainer='Chris Lalancette',
-    maintainer_email='clalancette@openrobotics.org',
-    author='Graylin Trevor Jay, Austin Hendrix',
-    keywords=['ROS'],
-    classifiers=[
-        'Intended Audience :: Developers',
-        'License :: BSD',
-        'Programming Language :: Python',
-        'Topic :: Software Development',
-    ],
-    description='A robot-agnostic teleoperation node to convert keyboard'
-                'commands to Twist messages.',
+    maintainer='bwuk',
+    maintainer_email='kevin.a.araujo@gmail.com',
+    description='Hokuyo controller package for ROS2',
     license='BSD',
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            'joint_state_publisher = joint_state_publisher.joint_state_publisher:main',
-            'robot = robot:main'
+            'robot = hokuyo.robot:main',
         ],
     },
 )
