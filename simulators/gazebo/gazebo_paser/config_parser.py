c711bbfa1c2c4effd209955d19a1fc837aad4bdb
Beaninator <bensonjbeck1@gmail.com>
2025-02-13 20:00:44 -0500

    added cmd line functions
    use
    python config_parser.py <file_name.sdf> feagi_config_template.json

--- simulators/gazebo/gazebo_paser/config_parser.py
+++ simulators/gazebo/gazebo_paser/config_parser.py
@@ -1,1 +1,35 @@
 import json
+import sys
+import os
+
+def main():
+    if len(sys.argv) != 3:
+        print("Incorrect usage please use python config_parser.py <filename.sdf> <feagiTemplate.json>")
+        return
+
+    #begin opening files
+    try:
+        with open(sys.argv[2], 'r') as config:
+            config_json = json.load(config)
+
+    except FileNotFoundError:
+        print("Couldn't open the feagi config template <" + sys.argv[2] + ">")
+        quit()
+
+    try:
+        with open(sys.argv[1], 'r') as sdf:
+            sdf_file = sdf.read()
+            
+    except FileNotFoundError:
+        print("Couldn't open the target sdf file <" + sys.argv[1] + ">");
+        quit()
+    
+    print(config_json)
+    print(sdf_file)
+    return
+
+if __name__ =="__main__":
+    main()
+
+
+
