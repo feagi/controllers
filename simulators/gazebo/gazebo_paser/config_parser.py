9caa1c975f3f25fc5dbb9fca25641a099f644a02
Beaninator <bensonjbeck1@gmail.com>
2025-02-19 15:47:56 -0500

    Added XML Tree Stripping
    excludes any XML elements not in the allow list

--- simulators/gazebo/gazebo_paser/config_parser.py
+++ simulators/gazebo/gazebo_paser/config_parser.py
@@ -3,6 +3,10 @@ import sys
 import os
 import xml.etree.ElementTree as ET
 
+# Description : used to parse the SDF to an XML structure which can be iterated through
+# INPUT : file path (String)
+# Output on success : XML tree
+# Output on fail : None
 def sdf_to_xml(fp):
     try:
         tree = ET.parse(fp)
@@ -14,16 +18,38 @@ def sdf_to_xml(fp):
         print(f"File couldn't be found : {fp}")
         return None
 
-def print_xml_tree(element, indent=0):
-    """Prints an XML element and its children in a tree format."""
-    print("  " * indent + "<" + element.tag + ">")
+# Description : used to strip the XML tree of any unnecessary elements
+# INPUT : tree element (expected to be the root)
+# Output on success : XML tree
+# Output on fail : None
+def strip_tree (element, found_elements): 
     for child in element:
-        print_xml_tree(child, indent + 1)
-    if element.text is not None and element.text.strip():
-         print("  " * (indent+1) + element.text.strip())
-    print("  " * indent + "</" + element.tag + ">")
+        #print(element.tag)
+        if element.tag in ALLOW_LIST:
+            found_elements.append(element)
+        else:
+            strip_tree(child, found_elements)
+            
+
+# !Ignore this, it's unimportant and was just used for testing! #
+def explore_tree(element):
+    tags = []
+    for child in element:
+        if element.tag not in tags:
+            tags.append(element.tag)
+        explore_tree(child)
+
+    return tags
+        #if child.text is not None and element.text.strip():
+
+
 
 def main():
+    global TRANSMISSION_TYPES
+    global SENSING_TYPES
+    global ALLOW_LIST 
+    global IGNORE_LIST 
+    
     # Requires 2 items <target sdf> and <gazebo_template.json>
     if len(sys.argv) != 3:
         print("Incorrect usage please use python config_parser.py <filename.sdf> <gazebo_template.json>")
@@ -33,21 +59,37 @@ def main():
     try:
         with open(sys.argv[2], 'r') as config:
             config_json = json.load(config)
+
+        try:
             TRANSMISSION_TYPES = config_json['actuator']
             SENSING_TYPES = config_json['sensor']
+            ALLOW_LIST = config_json['allow_list']
+            IGNORE_LIST = config_json['ignore_list']
 
-    except FileNotFoundError as e:
-        print(f"Couldn't open the gazebo config template <" + sys.argv[2] + ">\n{e}")
+        except KeyError as err :
+            print(f"Gazebo config seems invalid : \n{err}")
+
+    except FileNotFoundError as err:
+        print(f"Couldn't open the gazebo config template <" + sys.argv[2] + ">\n{err}")
         quit()
 
+    print("\n~Opened all files successfully~\n")
+    
     # Imports the sdf file as an xml structure to navigate through
     xml_tree = sdf_to_xml(sys.argv[1])
 
     # Prints out the elements in the tree
     root = xml_tree.getroot()
-    print_xml_tree(root)
 
-    print("\n~Opened all files successfully~\n")
+    # Will store all found elements
+    found_elements = []
+
+    # Grabs all allowed sdf elements (elements matching those in gazebo_config['allow_list'])
+    strip_tree(root, found_elements)
+
+    # At this point we found all elements we're looking for, now we begin parsing it into terms feagi can understand
+    print(found_elements)
+
     
     return
 
