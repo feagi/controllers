217bd14ed701da6575380195b0768a2d8519617c
Beaninator <bensonjbeck1@gmail.com>
2025-02-19 16:48:14 -0500

    Update config_parser.py

--- simulators/gazebo/gazebo_paser/config_parser.py
+++ simulators/gazebo/gazebo_paser/config_parser.py
@@ -25,72 +25,65 @@ def sdf_to_xml(fp):
 def strip_tree (element, found_elements): 
     for child in element:
         #print(element.tag)
-        if element.tag in ALLOW_LIST:
+        if element.tag in g_config['allow_list']:
             found_elements.append(element)
         else:
             strip_tree(child, found_elements)
             
+# Description : used to load all 3 necessary files (feagi template config, gazebo template config, and the target sdf file) 
+# INPUT : gazebo config file path, feagi config file path, target sdf file path, array to store found elements in
+# Output on success : Populates found_elements with all allowed elements from the sdf
+# Output on fail : None
+def open_files(gazebo_config_template, feagi_config_template, target_sdf, found_elements):
+    global g_config
+    global f_config
 
-# !Ignore this, it's unimportant and was just used for testing! #
-def explore_tree(element):
-    tags = []
-    for child in element:
-        if element.tag not in tags:
-            tags.append(element.tag)
-        explore_tree(child)
-
-    return tags
-        #if child.text is not None and element.text.strip():
-
-
-
-def main():
-    global TRANSMISSION_TYPES
-    global SENSING_TYPES
-    global ALLOW_LIST 
-    global IGNORE_LIST 
-    
-    # Requires 2 items <target sdf> and <gazebo_template.json>
-    if len(sys.argv) != 3:
-        print("Incorrect usage please use python config_parser.py <filename.sdf> <gazebo_template.json>")
-        return
-
-    # Loads gazebo template json component
     try:
-        with open(sys.argv[2], 'r') as config:
-            config_json = json.load(config)
-
-        try:
-            TRANSMISSION_TYPES = config_json['actuator']
-            SENSING_TYPES = config_json['sensor']
-            ALLOW_LIST = config_json['allow_list']
-            IGNORE_LIST = config_json['ignore_list']
-
-        except KeyError as err :
-            print(f"Gazebo config seems invalid : \n{err}")
+        with open(gazebo_config_template, 'r') as config:
+            g_config = json.load(config)
 
     except FileNotFoundError as err:
-        print(f"Couldn't open the gazebo config template <" + sys.argv[2] + ">\n{err}")
+        print(f"Couldn't open the gazebo config template <" + gazebo_config_template + ">\n{err}")
         quit()
 
-    print("\n~Opened all files successfully~\n")
-    
-    # Imports the sdf file as an xml structure to navigate through
-    xml_tree = sdf_to_xml(sys.argv[1])
+    try:
+        with open(feagi_config_template, 'r') as config:
+            f_config = json.load(config)
+    except FileNotFoundError as err:
+        print(f"Couldn't open the feagi config template <" + feagi_config_template + ">\n{err}")
 
-    # Prints out the elements in the tree
-    root = xml_tree.getroot()
+    print("Opened all files successfully...")
+
+    tree = sdf_to_xml(target_sdf)
+    root = tree.getroot()
+    strip_tree(root, found_elements)
+
+def main():
+    # CMD LINE USAGE :
+    # 1 - python config_parser.py <target.sdf> 
+    #
+    #       * Uses default gazebo config : 'gazebo_config_template.json'
+    #       * Uses default feagi config : 'feagi_config_template.json'
+    #
+    # 2 - python config_parser.py <target.sdf> <gazebo_config.json> <feagi_config.json> 
+    #
+    #   * Both <gazebo_config.json> and <feagi_config.json> or the default files must be in the current directory to work properly *
 
     # Will store all found elements
     found_elements = []
 
-    # Grabs all allowed sdf elements (elements matching those in gazebo_config['allow_list'])
-    strip_tree(root, found_elements)
+    num_args = len(sys.argv) - 1
 
-    # At this point we found all elements we're looking for, now we begin parsing it into terms feagi can understand
-    print(found_elements)
-
+    if num_args == 1:
+        print(sys.argv[1] + '\n')
+        open_files('gazebo_config_template.json', 'feagi_config_template.json', sys.argv[1], found_elements)
+    elif num_args == 3:
+        open_files(sys.argv[2], sys.argv[3], sys.argv[1], found_elements)
+    else :
+        print("Incorrect command usage, please use either :\npython config_parser.py <target.sdf> <gazebo_config.json> <feagi_config.json>\npython config_parser.py <target.sdf>")
+        return
     
+    print(found_elements)
     return
 
 if __name__ =="__main__":
